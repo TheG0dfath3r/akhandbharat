2a24d387a8c5ce9f1c8033c14e6e8ec692e6a5a1
KUNAL singh <kunal.singh2018@vitstudent.ac.in>
2020-09-22 00:08:10 +0530

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Map, Marker, Popup, TileLayer } from "react-leaflet"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import "./app.css"
+
+export default function App() {
+  return (
+    <Map center={[29.0588, 76.0856]} zoom={9}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </Map>
+  )
 }
