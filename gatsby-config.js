56eb508207607490268b54db68d29217d56b0cf8
KUNAL singh <kunal.singh2018@vitstudent.ac.in>
2020-09-22 16:08:56 +0530

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,3 +8,5 @@ module.exports = {
   /* Your site config here */
   plugins: [],
 }
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,15 @@ import "./app.css"
 
 export default function App() {
   return (
-    <Map center={[29.0588, 76.0856]} zoom={9}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </Map>
+    <>
+      {typeof window !== "undefined" && (
+        <Map center={[29.0588, 76.0856]} zoom={9}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </Map>
+      )}
+    </>
   )
 }
